d0d4f1e679f6d12032d0a4f616821833bb3845f4
anacanestraro <ana.canestraro07@gmail.com>
2023-12-14 16:52:39 -0300

    não sei mais

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // getAll
-const getAllPosts = async (artist, album) => {
+const getAllPosts = async (album) => {
     try {
-        const res = await fetch(`http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=05bd3517a04dadff15290acb4341232c&artist=${artist}&album=${album}&format=json`);
+        const res = await fetch(`http://ws.audioscrobbler.com/2.0/?method=album.search&album=$(album)&api_key=05bd3517a04dadff15290acb4341232c&format=json`);
         const data = await res.json();
 
         if (!res.ok) {
@@ -15,14 +15,14 @@ const getAllPosts = async (artist, album) => {
         
         artistas.forEach((element) => {
             console.log(
-                `Nome artista: ${element.artist} \n Nome do album: ${element.album}`
+                `Nome artista: ${element.name} \n Nome do album: ${element.album}`
             );
             var container = document.querySelector(".containercards");
             let card = `
             <div class="card" style="width: 18rem;">
-            <img src="" class="card-img-top" alt="Foto artista ${element.artist}">
+            <img src="" class="card-img-top" alt="Foto artista ${element.name}">
             <div class="card-body">
-            <h5 class="card-title">${element.artist}</h5>
+            <h5 class="card-title">${element.name}</h5>
                     <p class="card-text">Album:${element.album}</p>
                     <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
@@ -39,7 +39,7 @@ const getAllPosts = async (artist, album) => {
 
 const createPost = async (novaBuscaJSON) => {
     try {
-      const res = await fetch(`https://jhttp://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=05bd3517a04dadff15290acb4341232c&artist=${artist}&album=${album}&format=jsonsonplaceholder.typicode.com/posts`, {
+      const res = await fetch(`http://ws.audioscrobbler.com/2.0/?method=album.search&album=believe&api_key=05bd3517a04dadff15290acb4341232c&format=json`, {
         method: "POST",
         headers: {
           "Content-type": "application/json; charset=UTF-8",
@@ -61,9 +61,8 @@ const createPost = async (novaBuscaJSON) => {
   };
   
   let novaBusca = {
-    userId: 1,
-    title: "Novo Post",
-    body: "quia et suscipitsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totamnostrum rerum est autem sunt rem eveniet architecto",
+    name: "paramore",
+    album:"this is why"
   };
   
   let novaBuscaJSON = JSON.stringify(novaBusca);
